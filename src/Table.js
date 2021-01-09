44cb281ea07a79bfd4fd2f82e2d64f3cedaf5971
skylark <sekurth@calpoly.edu>
2021-01-08 23:17:40 -0800

    Assgn1 pt 4 commit

--- src/Table.js
+++ src/Table.js
@@ -18,6 +18,9 @@ function TableBody (props) {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -28,13 +31,14 @@ function TableBody (props) {
    );
 }
 
+
 function Table(props) {
-   return(
-      <table>
-        <TableHeader/>
-        <TableBody characterData={props.characterData} />
-      </table>
-   );
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 
