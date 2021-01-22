c9e24df6dc761599b2fb368053b91c4c9b3f01fd
sky-kurth <sekurth@calpoly.edu>
2021-01-21 23:35:39 -0800

    Linked backend?

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,29 +1,83 @@
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
-   const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+         setCharacters(result);
+    });
+  }, [] );
 
-    function updateList(person) {
-	  setCharacters([...characters, person]);
-	}
 
-    function removeOneCharacter (index) {
-      const updated = characters.filter((character, i) => {
+
+  function removeOneCharacter (index) {
+    
+    makeDeleteCall(characters[index].id).then(( result => {
+      if (result.status === 204){
+        const updated = characters.filter((character, i) => {
          return i !== index
-      });
-      setCharacters(updated);
+        });
+        setCharacters(updated);
+      }
+    }));
+  }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    // if (result)
+    //   setCharacters([...characters, person] );
+    // });
+    if (result.status === 201)
+      setCharacters([...characters, result.data] );
+    });
+  }
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
     }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+  }
 
-   return (
-   	<div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
-    </div>
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makeDeleteCall(id){
+    try {
+      const response = await axios.delete('http://localhost:5000/users/' + id);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  return (
+ 	<div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form handleSubmit={updateList} />
+  </div>
   ) 
-
-
 
 }
 
--- src/Table.js
+++ src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Job</th>
       </tr>
@@ -16,8 +17,12 @@ function TableBody (props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+        <td>{row.id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -32,7 +37,7 @@ function Table(props) {
    return(
       <table>
         <TableHeader/>
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
       </table>
    );
 }
